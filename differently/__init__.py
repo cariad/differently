8a85f635ec17ecb22d802c2261a1edd3f2bdac13
Cariad Eccleston <cariad@cariad.io>
2021-08-31 20:55:44 +0100

    Add get_version

--- differently/__init__.py
+++ differently/__init__.py
@@ -1,7 +1,9 @@
 from differently.list import ListDifferently
 from differently.text import TextDifferently
+from differently.version import get_version
 
 __all__ = [
+    "get_version",
     "ListDifferently",
     "TextDifferently",
 ]
