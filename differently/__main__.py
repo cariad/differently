18f7ab6cff28f96099b8bd6e030e5e443425c37f
Cariad Eccleston <cariad@cariad.io>
2021-08-31 19:52:29 +0100

    Add __main__.py

---
+++ differently/__main__.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+from colorama import deinit, init
+
+from differently import TextDifferently
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="differently")
+    parser.add_argument("file1", help="File 1")
+    parser.add_argument("file2", help="File 2")
+    args = parser.parse_args()
+
+    init()
+    with open(args.file1, "r") as file1:
+        with open(args.file2, "r") as file2:
+            print(TextDifferently(file1.read(), file2.read()))
+    deinit()
