3cbb79d92bebf7af0391aa7fb9df091ca9ca31c5
Cariad Eccleston <cariad@cariad.earth>
2021-10-23 15:22:49 +0100

    Add render

--- differently/handlers/__init__.py
+++ differently/handlers/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Type
+from typing import IO, Any, Callable, Dict, List, Tuple, Type
 
+from differently.exceptions import DeserializationError
 from differently.handlers.json import JsonDifferently
 from differently.handlers.list import ListDifferently
 from differently.handlers.text import TextDifferently
@@ -30,6 +31,26 @@ def deserialize(keys: str, index: int, path: Path) -> Any:
     return loaders[key](path)
 
 
+def deserialize_value(v: str) -> Tuple[Type[ListDifferently], Any]:
+    """
+    Attempts to deserialise `v`.
+
+    Returns the deserialiser and the deserialised value.
+    """
+
+    try:
+        return YamlDifferently[Any], YamlDifferently.load(v)
+    except DeserializationError:
+        pass
+
+    try:
+        return JsonDifferently[Any], JsonDifferently.load(v)
+    except DeserializationError:
+        pass
+
+    return TextDifferently, v
+
+
 def get_deserializer_keys() -> List[str]:
     """Gets the keys of all available deserialisers."""
     return [key for key in loaders]
@@ -43,6 +64,18 @@ def get_renderer(key: str) -> Type[ListDifferently]:
 def get_renderer_keys() -> List[str]:
     """Gets the keys of all available renderers."""
     return [key for key in renderers]
+
+
+def render(a: str, b: str, writer: IO[str]) -> None:
+    """
+    Renders the differences between `a` and `b` to `writer`.
+
+    Treats the inputs as marked-up data if possible.
+    """
+
+    _, da = deserialize_value(a)
+    t, db = deserialize_value(b)
+    writer.write(str(t(da, db)))
 
 
 __all__ = [
