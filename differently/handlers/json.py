ae4feeade1e37daaa1f4368c47c2a8781f152f2c
Cariad Eccleston <cariad@cariad.earth>
2021-10-23 15:23:12 +0100

    Add support for string loading

--- differently/handlers/json.py
+++ differently/handlers/json.py
@@ -1,7 +1,8 @@
-from json import dumps, load
+from json import dumps, load, loads
 from pathlib import Path
-from typing import Generic, List, Optional, cast
+from typing import Generic, List, Optional, Union, cast
 
+from differently.exceptions import DeserializationError
 from differently.handlers.list import ListDifferently
 from differently.types import TComparable
 
@@ -22,9 +23,22 @@ class JsonDifferently(ListDifferently, Generic[TComparable]):
         )
 
     @staticmethod
-    def load(path: Path) -> TComparable:
-        with open(path, "r") as f:
-            return cast(TComparable, load(f))
+    def load(i: Union[Path, str]) -> TComparable:
+        """
+        If `i` is a `Path` then deserialises the file at that location as JSON.
+
+        If `i` is a `str` then deserialises `i` as JSON.
+
+        Raises `DeserializationError` if deserialisation fails.
+        """
+
+        try:
+            if isinstance(i, str):
+                return cast(TComparable, loads(i))
+            with open(i, "r") as f:
+                return cast(TComparable, load(f))
+        except Exception as ex:
+            raise DeserializationError(ex)
 
     @staticmethod
     def to_strings(d: TComparable) -> List[str]:
